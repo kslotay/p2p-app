cd0796eca15d8330a9b7ef6725e8d7186460718b
cyzanfar <cghazanfar10@gmail.com>
2018-11-14 02:15:24 -0500

    next

--- main.cc
+++ main.cc
@@ -78,7 +78,7 @@ void ChatDialog::processMessage(QByteArray datagramReceived)
 	if (messageMap.contains("ChatText")) {
 		qDebug() << "message contains chattext";
 
-		textview->append(messageMap.key("ChatText"));
+		textview->append(messageMap.value());
 
 	}
 
